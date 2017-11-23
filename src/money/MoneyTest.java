185f4ac601d641c7a627004f7550d4a522fadaac
JunichiSato <sato440nomusuko@gmail.com>
2017-11-23 20:10:51 +0900

    テストコードの誤りを修正(p14で混入）

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -10,9 +10,9 @@ class MoneyTest {
 	void testMultiplication() {
 		Dollar five = new Dollar(5);
 		Dollar product = five.times(2);
-		assertEquals(10, product);
+		assertEquals(10, product.amount);
 		product = five.times(3);
-		assertEquals(15, product);
+		assertEquals(15, product.amount);
 	}
 	@Test
 	void testEqulity() {
